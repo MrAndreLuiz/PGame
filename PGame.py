7c7c42439a908367fa9840843c7004bd6f626f75
André Luiz <andre.luiz.silva01@hotmail.com>
2016-08-18 14:50:31 -0300

    Implementação das imagens, texturas e sons iniciais
    Implementação das imagens, texturas e sons iniciais da base, do
    personagem, do fundo e da mensagem.

--- PGame.py
+++ PGame.py
@@ -8,6 +8,18 @@ from pygame.locals import *
 FPS = 30
 SCREENWIDTH = 288
 SCREENHEIGHT = 512
+# imagem dicts
+IMAGES = {}
+
+# tupla com as tres posicoes da barbatana
+PLAYER_LIST = (
+    'assets/images/redfish-upfin.png',
+    'assets/images/redfish-midfin.png',
+    'assets/images/redfish-downfin.png',
+)
+
+# fundo
+BACKGROUND = 'assets/images/background.png'
 
 def main():
     global SCREEN, FPSCLOCK
@@ -15,6 +27,21 @@ def main():
     FPSCLOCK = pygame.time.Clock()
     SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
     pygame.display.set_caption('PGame')
+
+    IMAGES['welcome'] = pygame.image.load('assets/images/welcome.png').convert_alpha()
+    # imagem da base (terreno)
+    IMAGES['base'] = pygame.image.load('assets/images/base.png').convert_alpha()
+
+    IMAGES['background'] = pygame.image.load(BACKGROUND).convert()
+
+    # seleciona imagens aleatorias do personagem
+    randPlayer = random.randint(0, len(PLAYER_LIST) - 1)
+    IMAGES['player'] = (
+        pygame.image.load(PLAYER_LIST[randPlayer]).convert_alpha(),
+        pygame.image.load(PLAYER_LIST[randPlayer]).convert_alpha(),
+        pygame.image.load(PLAYER_LIST[randPlayer]).convert_alpha(),
+    )
+
     pygame.display.update()
     FPSCLOCK.tick(FPS)
 
