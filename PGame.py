54f2281068a32b18d6a407f26fcfa07f589e8adf
André Luiz <andre.luiz.silva01@hotmail.com>
2016-08-27 15:24:36 -0300

    Implementação da função de jogabilidade
    Implementação da função de jogabilidade, movimentação dos personagens,
    estruturação dos troncos e condições para tratamento da jogabilidade.

--- PGame.py
+++ PGame.py
@@ -71,7 +71,7 @@ def main():
 
 def initialAnimation():
     """Mostra a animacao inicial"""
-    # indice do personagem no blit da tela
+    # indice dos personagens no blit da tela
     playerIndex = 0
     playerIndexGen = cycle([0, 1, 2, 1])
     # iterator usado para mudar playerIndex apos a quinta interacao
@@ -87,7 +87,7 @@ def initialAnimation():
     # montante do desvio maximo da base para a esquerda
     baseShift = IMAGES['base'].get_width() - IMAGES['background'].get_width()
 
-    # shm do personagem para o movimento de cima para baixo na tela inicial
+    # shm dos personagens para o movimento de cima para baixo na tela inicial
     playerShmVals = {'val': 0, 'dir': 1}
 
     while True:
@@ -113,6 +113,100 @@ def initialAnimation():
         pygame.display.update()
         FPSCLOCK.tick(FPS)
         # testes de desempenho
+        printFPS()
+
+
+def mainGame(movementInfo):
+    playerIndex = loopIter = 0
+    playerIndexGen = movementInfo['playerIndexGen']
+    playerx, playery = int(SCREENWIDTH), movementInfo['playery']
+
+    basex = movementInfo['basex']
+    baseShift = IMAGES['base'].get_width()
+
+    # pega 2 novos troncos para adicionar para as listas de upperTrunks lowerTrunks
+    newTrunk1 = 1
+    newTrunk2 = 1
+
+    # lista de troncos superiores
+    upperTrunks = [
+        {'x': SCREENWIDTH + 200, 'y': newTrunk1[0]['y']},
+        {'x': SCREENWIDTH, 'y': newTrunk2[0]['y']},
+    ]
+
+    # lista de troncos inferiores
+    lowerTrunks = [
+        {'x': SCREENWIDTH + 200, 'y': newTrunk1[1]['y']},
+        {'x': SCREENWIDTH, 'y': newTrunk2[1]['y']},
+    ]
+
+    trunkVelX = -4
+
+    # velocidade dos personagens, velocidade maxima, aceleracao de queda, aceleracao da nadadeira
+    playerVelY    = 5
+    playerMaxVelY = 18
+    playerMinVelY = -14
+    playerAccY    = 1
+    playerFinAcc = -9
+    playerFinped = False
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+        # verifica acidentes
+        crashTest = "lower"
+        if crashTest[0]:
+            return {
+                'y': playery,
+                'groundCrash': crashTest[1],
+                'basex': basex,
+                'upperTrunks': upperTrunks,
+                'lowerTrunks': lowerTrunks,
+                'playerVelY': playerVelY,
+            }
+
+        # mudancas em playerIndex basex
+        if (loopIter + 1) % 3 == 0:
+            playerIndex = playerIndexGen.next()
+        loopIter = (loopIter + 1) % 30
+        basex = -((-basex + 100) % baseShift)
+
+        # movimento dos personagens
+        if playerVelY < playerMaxVelY and not playerFinped:
+            playerVelY += playerAccY
+        if playerFinped:
+            playerFinped = False
+        playerHeight = IMAGES['player'][playerIndex].get_height()
+        playery += min(playerVelY, BASEY - playery - playerHeight)
+
+        # move os troncos para a esquerda
+        for uTrunk, lTrunk in zip(upperTrunks, lowerTrunks):
+            uTrunk['x'] += trunkVelX
+            lTrunk['x'] += trunkVelX
+
+        # adiciona novo tronco quando o primeiro tronco esta prestes a tocar a esquerda da tela
+        if 0 < upperTrunks[0]['x'] < 5:
+            newTrunk = 1
+            upperTrunks.append(newTrunk[0])
+            lowerTrunks.append(newTrunk[1])
+
+        # desenha as imagens
+        SCREEN.blit(IMAGES['background'], (0,0))
+
+        for uTrunk, lTrunk in zip(upperTrunks, lowerTrunks):
+            SCREEN.blit(IMAGES['trunk'][0], (uTrunk['x'], uTrunk['y']))
+            SCREEN.blit(IMAGES['trunk'][1], (lTrunk['x'], lTrunk['y']))
+
+        SCREEN.blit(IMAGES['base'], (basex, BASEY))
+        SCREEN.blit(IMAGES['player'][playerIndex], (playerx, playery))
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+        # imprime o frame rate para testes de desempenho
         printFPS()
 
 
